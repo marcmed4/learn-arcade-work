7186bc1a6d92f7add7593db8157c67069a12abac
Marc Medici <marc.medici@my.simpson.edu>
2020-08-30 14:18:08 -0500

    Lab 1

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,4 +1,5 @@
-hello
 print("hello world")
 print("Ben is wearing a blue shirt. I like it.")
 print("I like to run.")
+print("I asked Ben if he wanted to go running. He responded: \"no way\"")
+print("\t its weird he does not like to run")
